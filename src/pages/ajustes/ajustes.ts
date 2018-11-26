26281e256375d028eed3a27d9aff141e2282c08c
Guille <info@guillegonzarod.com>
2018-11-26 23:57:08 +0100

    Modal - Crear, enviar y recibir parámetros.

--- src/pages/ajustes/ajustes.ts
+++ src/pages/ajustes/ajustes.ts
@@ -1,5 +1,6 @@
+import { ModalPage } from './../modal/modal';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -8,10 +9,27 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AjustesPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    public modalCtrl: ModalController) {
   }
 
   activarPrincipal() {
     this.navCtrl.parent.select(0);
   }
+
+  mostrar_modal() {
+    let modal = this.modalCtrl.create(ModalPage, { nombre: "Guille", edad: 10 });
+    modal.present();
+
+    modal.onDidDismiss(parametros => {
+      if(parametros){
+        console.log("Datos del modal:");
+        console.log(parametros);
+      }else{
+        console.log("Se cerró el modal sin parámetros");
+      }
+
+    });
+  }
+
 }
