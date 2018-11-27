f9f2e069fd3e7f7c4a92658f272bed2cf8a1a059
Guille <info@guillegonzarod.com>
2018-11-27 23:43:16 +0100

    Mostrar y ocultar el menú lateral desde eventos y atributos HTML.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Ajustes2Page } from './../pages/ajustes2/ajustes2';
 
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, MenuController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -18,7 +18,8 @@ export class MyApp {
 
   rootPage: any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
+    private menuCtrl: MenuController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -29,6 +30,7 @@ export class MyApp {
 
   public abrirPagina($event, pagina: Page): void {
     this.rootPage = pagina;
+    this.menuCtrl.close();
   }
 
 }
--- src/pages/principal/principal.ts
+++ src/pages/principal/principal.ts
@@ -1,6 +1,6 @@
 import { Pagina2Page } from './../pagina2/pagina2';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, MenuController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -9,12 +9,17 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class PrincipalPage {
 
-  pagina2:any = Pagina2Page;
+  pagina2: any = Pagina2Page;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) { }
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    public menuCtrl: MenuController) { }
 
   navegarPagina() {
     this.navCtrl.push(Pagina2Page);
+  }
+
+  mostrarMenu() {
+    this.menuCtrl.toggle();
   }
 
 }
